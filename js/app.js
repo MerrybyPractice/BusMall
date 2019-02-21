c9c1bdd82889e0e97195f4169db198d84556c976
Mary Cimakasky <MaryCimakasky@Marys-MacBook-Pro.local>
2019-02-20 19:44:36 -0800

    havent done this in a while, oops

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+//ideas:
+//  a 'stable array' that does not get shuffled and is used for rendering the tables and storeing the data
 //global variables
 var total_clicks = 25;
 var product_block = document.getElementById('product_block');
@@ -11,13 +13,18 @@ var left_title = document.getElementById('h2_left');
 var results_list = document.getElementById('results_list');
 var product_array = [];
 var last_array = [];
+var handler_array = [];
 var label_array = [];
 var clicks_array = [];
 var chart_button = document.getElementById('chart_button');
 var click_counter = 0;
+var my_bar_chart;
+var my_pie_chart;
+
 //constructor function
-var Product = function (name,file_path,id){
+var Product = function (name,number,file_path,id){
   this.name = name;
+  this.number = number;
   this.img = file_path;
   this.shown = 0;
   this.id = id;
@@ -40,26 +47,26 @@ function shuffle(array) {
 }
 
 function fill_product_array(){
-  new Product('R2D2 Suitcase', 'img/bag.jpg', 'r2d2_bag');
-  new Product('Banana Slicer', 'img/banana.jpg', 'banana_slicer');
-  new Product('Bathroom IPad Stand', 'img/bathroom.jpg', 'bathroom_stand');
-  new Product('Rain Sandals', 'img/boots.jpg', 'rain_sandals');
-  new Product('All in One Breakfast', 'img/breakfast.jpg', 'breakfast');
-  new Product('Meatball Bubble Gum (Italian Style)', 'img/bubblegum.jpg', 'meat_gum');
-  new Product('Round Bottomed Chair', 'img/chair.jpg', 'round_chair');
-  new Product('Cthulhu Wins! Action Figure', 'img/cthulhu.jpg', 'cthulhu');
-  new Product('Dog Beak', 'img/dog-duck.jpg', 'dog_beak');
-  new Product( 'Dragon Meat','img/dragon.jpg','dragon');
-  new Product('Utensil Pen', 'img/pen.jpg', 'pen');
-  new Product('Pet Sweep', 'img/pet-sweep.jpg', 'pet_sweep');
-  new Product('Pizza Scissors', 'img/scissors.jpg','pizza_scissors');
-  new Product('Shark-Sleeper', 'img/shark.jpg', 'shark_sleep');
-  new Product('My First Floor Mop', 'img/sweep.png','sweep');
-  new Product('Gut-Your-Own Tauntaun', 'img/tauntaun.jpg','tauntaun');
-  new Product ('Unicorn Meat', 'img/unicorn.jpg', 'unicorn_meat');
-  new Product ('Life-like Wiggling Octo-Arm!', 'img/usb.gif', 'octo_usb');
-  new Product('Futility', 'img/water-can.jpg', 'water-can');
-  new Product('The All Nose Wine Glass', 'img/wine-glass.jpg', 'wine-glass');
+  new Product('R2D2 Suitcase', 14, 'img/bag.jpg', 'r2d2_bag'); //borken
+  new Product('Banana Slicer', 2,'img/banana.jpg', 'banana_slicer');
+  new Product('Bathroom IPad Stand', 3,'img/bathroom.jpg', 'bathroom_stand');
+  new Product('Rain Sandals', 15,'img/boots.jpg', 'rain_sandals');
+  new Product('All in One Breakfast', 1, 'img/breakfast.jpg', 'breakfast');
+  new Product('Meatball Bubble Gum (Italian Style)', 10, 'img/bubblegum.jpg', 'meat_gum'); //broken
+  new Product('Round Bottomed Chair', 16, 'img/chair.jpg', 'round_chair');
+  new Product('Cthulhu Wins! Action Figure', 4,'img/cthulhu.jpg', 'cthulhu');
+  new Product('Dog Beak', 5, 'img/dog-duck.jpg', 'dog_beak');
+  new Product( 'Dragon Meat', 6, 'img/dragon.jpg', 'dragon');
+  new Product('Utensil Pen', 20,'img/pen.jpg', 'pen');
+  new Product('Pet Sweep', 12, 'img/pet-sweep.jpg', 'pet_sweep');
+  new Product('Pizza Scissors',13, 'img/scissors.jpg','pizza_scissors');
+  new Product('Shark-Sleeper', 17,'img/shark.jpg', 'shark_sleep'); //broken
+  new Product('My First Floor Mop', 11, 'img/sweep.png','sweep');
+  new Product('Gut-Your-Own Tauntaun', 8, 'img/tauntaun.jpg','tauntaun');
+  new Product ('Unicorn Meat', 19,'img/unicorn.jpg', 'unicorn_meat');
+  new Product ('Life-like Wiggling Octo-Arm!', 9, 'img/usb.gif', 'octo_usb');
+  new Product('Futility', 7, 'img/water-can.jpg', 'water-can');
+  new Product('The All Nose Wine Glass', 18,'img/wine-glass.jpg', 'wine-glass');
 }
 fill_product_array();
 
@@ -71,7 +78,6 @@ function fill_chart_arrays(){
   }
 
 }
-
 
 var product_right = product_array[random_product()];
 var product_center = product_array[random_product()];
@@ -87,6 +93,11 @@ function getRandomColor(){
   return color;
 }
 
+function stringify_array(){
+  var stringy_product = JSON.stringify(product_array);
+  localStorage.setItem('product_details', stringy_product);
+}
+
 //randomizer
 function random_product(){
 
@@ -94,6 +105,9 @@ function random_product(){
 }
 function end_showing(){
   if(total_clicks <= 0){
+
+    product_array.concat(handler_array);
+    product_array.concat(last_array);
     product_block.removeEventListener('click', count_clicks);
     right_img.src = null;
     left_img.src = null;
@@ -111,6 +125,7 @@ function end_showing(){
   if(total_clicks <= 0){
     fill_chart_arrays();
     render_bar_chart();
+    stringify_array();
   }
 
 }
@@ -138,26 +153,33 @@ var count_clicks = function (event){
       product_left.clicked ++;
   }
 
-  var handler_array = [];
+  handler_array = [];
   product_array = shuffle(product_array);
   handler_array.push(product_array.pop());
   handler_array.push(product_array.pop());
   handler_array.push(product_array.pop());
+  last_array=[...handler_array];
   product_array = product_array.concat(last_array);
-
   product_left=handler_array[0];
   product_center=handler_array[1];
   product_right=handler_array[2];
 
-  last_array=handler_array;
+  //spread
   render_images();
   end_showing();
+  // stringify_array();
 };
 //render charts
 function render_bar_chart(){
   var ctx = document.getElementById('myChart').getContext('2d');
   // eslint-disable-next-line
-  var myChart = new Chart(ctx, {
+  Chart.scaleService.updateScaleDefaults('linear', {
+    ticks: {
+      min: 0
+    }
+  });
+  // eslint-disable-next-line no-undef
+  my_bar_chart = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: label_array,
@@ -238,11 +260,11 @@ function render_bar_chart(){
     }
   });
 }
-
 function render_pie_chart(){
+
   var ctx = document.getElementById('myChart').getContext('2d');
   // eslint-disable-next-line
-  var myChart = new Chart(ctx, {
+  my_pie_chart = new Chart(ctx, {
     type: 'pie',
     data: {
       labels: label_array,
@@ -323,20 +345,31 @@ function render_pie_chart(){
     }
   });
 }
+
+//swap chart views
 function pressed_chart_button(event){
   click_counter++;
   if (event.target.id==='chart_button' && click_counter %2 === 1){
+    my_bar_chart.destroy();
     render_pie_chart();
   }else if (event.target.id ==='chart_button'&& click_counter %2 === 0){
+    my_pie_chart.destroy();
     render_bar_chart();
   }
 }
-console.log('click');
+
+// check_local_storage();
 //event lisitiner
 product_block.addEventListener('click', count_clicks);
 chart_button.addEventListener('click', pressed_chart_button);
 render_images();
 
 //init
+if(localStorage.getItem('product_details')){
+  var stringy_product = localStorage.getItem('product_details');
+  product_array = JSON.parse(stringy_product);
+}
 
+console.log(clicks_array);
+console.log(product_array);
 
